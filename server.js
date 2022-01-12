acaab07fdd0894396991a0580d62747d45b9d3ef
Your Name <rozovziv1@gmail.com>
2022-01-13 00:01:39 +0200

    Erase now

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
+const express = require('express');
+const mongoose = require('mongoose');
+const pokemonRouter = require('./routes/pokemon');
+const app = express();
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
+mongoose.connect('mongodb://localhost/pokemon');
 
-const indexRouter = require('./routes/index')
-const pokemonRouter = require('./routes/pokemon')
+app.set('view engine', 'ejs');
+app.use('/pokemon', pokemonRouter);
+app.get('/', (req, res) => {
+  const pokemon = [
+    {
+      title: 'Test Pokemon',
+      createdAt: new Date(),
+      description: 'Test Description',
+    },
+  ];
+  res.render('pokemon/index', { pokemon });
+});
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
-
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
-const db = mongoose.connection
-db.on('error', error => {
-  console.error(error)
-})
-
-db.once('open', () => {
-  console.log('Connected to Mongoose')
-})
-
-app.use('/', indexRouter)
-app.use('/pokemon', pokemonRouter)
-
-app.listen(process.env.PORT || 3000)
+app.listen(5000);
