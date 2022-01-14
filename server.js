3ad8db37fb3965689b5e39503a36200379c32997
Your Name <rozovziv1@gmail.com>
2022-01-14 02:46:49 +0200

    render test

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use('/pokemon', pokemonRouter)
 
 app.get('/', async (req, res) => {
   const pokemon = await Pokemon.find().sort({ createdAt: 'desc' })
-  res.render('pokemon/index', { pokemon })
+  res.render('/', { pokemon })
 })
 
 app.listen(process.env.PORT || 3000)
